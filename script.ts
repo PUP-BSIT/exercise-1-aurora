b10ecacd616f065bd6e64213b44cca66041bc56c
Jasmin <jasminferolino083@gmail.com>
2024-09-24 19:12:28 +0800

    add tablet item

--- script.ts
+++ script.ts
@@ -13,3 +13,18 @@ type Item = {
     price: number;
     print(): void;
 };
+
+const tablet: Item = {
+    brand: "Samsung",
+    color: "Silver",
+    quantity: 2,
+    price: 35000,
+    print() {
+        console.log(`I wanna buy a ${this.brand}, since it is 
+        a known local brand. \n
+        I want it to be ${this.color}, since I like a cool and 
+        aesthetic look. \n
+        I want ${this.quantity} of this item. My budget is around 
+        ${this.price}. `);
+    },
+};
